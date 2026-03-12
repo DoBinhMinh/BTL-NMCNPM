c2f24b44e68155dead0cc32f9587394bd2310c03
DoBinhMinh <dominhptit12@gmail.com>
2026-03-12 13:55:11 +0700

    Tích hợp PUG

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 //dùng require thay cho import để nhúng thư viện express
 const express = require('express')
+
+//nhúng thư viện path có sẵn trong nodejs
+const path = require('path')
+
 //khởi tạo dự án app express()
 const app = express()
+
 //tên cổng
 const port = 3000
-//get để lấy ra gì đó ?  '/' là đại diện cho trang chủ
+
+//Thiết lập cho dự án biết thư mục chứa code cho phần giao diện views
+app.set('views', path.join(__dirname,"views"))
+
+//Thiết lập view engine
+app.set('view engine', 'pug')
+
+//get để lấy ra gì đó ?  '/' là đại diện cho trang chủ. 
+//Thay bằng render để chuyển pug thành html rồi render ra giao diện
 //biến req là dữ liệu fe gửi lên be
 //res là dữ liệu be phản hồi về cho fe
 app.get('/', (req, res) => {
-  res.send('Trang chủ')
+  res.render('client/pages/home',{
+    pageTitle: "Trang chủ"
+  })
 })
 
 app.get('/tours', (req, res) => {
-  res.send('Danh sách tour')
+  res.render('client/pages/tour-list',{
+    pageTitle: "Danh sách tour"
+  })
 })
+
 
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`)
